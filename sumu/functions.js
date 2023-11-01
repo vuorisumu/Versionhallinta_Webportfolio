572a8c792215340a17692a9c31c6dd4289246eea
Sumu Vuori <sumu.vuori@tuni.fi>
2023-11-01 10:14:45 +0200

    Add highlight functionality to links

--- sumu/functions.js
+++ sumu/functions.js
@@ -6,16 +6,37 @@ menubutton.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
 
+// scroll functionality
 const header = document.getElementById("header");
 const logo = document.getElementById("logo");
 const originalHeight = header.style.getPropertyValue("height");
+const sections = document.querySelectorAll("section[id]");
 window.onscroll = function () {
   scrollFunction();
 };
 function scrollFunction() {
+  // shrink header when scrolling
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     header.style.height = "20px";
   } else {
     header.style.height = originalHeight;
   }
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+    if (
+      window.scrollY > sectionTop &&
+      window.scrollY <= sectionTop + sectionHeight
+    ) {
+      document
+        .querySelector("#nav a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector("#nav a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
+  });
 }
