8b44a8b218d20c02d0f7dbfa77ebf7e7c5bbec66
Sumu Vuori <sumu.vuori@tuni.fi>
2023-11-01 10:05:18 +0200

    Shrink header when scrolling

--- sumu/functions.js
+++ sumu/functions.js
@@ -1,6 +1,21 @@
+// open and close navigation menu
 const menubutton = document.getElementById("hamburger");
 const menu = document.getElementById("nav");
 menubutton.addEventListener("click", () => {
   menubutton.classList.toggle("active");
   menu.classList.toggle("active");
 });
+
+const header = document.getElementById("header");
+const logo = document.getElementById("logo");
+const originalHeight = header.style.getPropertyValue("height");
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    header.style.height = "20px";
+  } else {
+    header.style.height = originalHeight;
+  }
+}
