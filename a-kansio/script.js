71430c6f02e12d82a696edb92dc10c68bd015895
apeura <anni.peura@tuni.fi>
2023-11-11 22:35:48 +0200

    script.js: Add existing script.

---
+++ a-kansio/script.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Store initial content
+  var initialContent = "Hello, hello, hello";
+  var boxContentElement = document.getElementById("boxContent");
+
+  // Function to change the content
+  function changeContent(newContent) {
+    boxContentElement.innerHTML = newContent;
+  }
+
+  // Add click event listeners to the links
+  document.getElementById("link1").addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent the default link behavior (page reload)
+    changeContent("bye bye bye");
+  });
+
+  document.getElementById("link2").addEventListener("click", function (event) {
+    event.preventDefault();
+    changeContent("goodbye, farewell, adieu");
+  });
+
+  document.getElementById("link3").addEventListener("click", function (event) {
+    event.preventDefault();
+    changeContent("see you later");
+  });
+
+  // Set initial content
+  changeContent(initialContent);
+});
+// Function to add the fade-out class to .hero_img
+function addFadeOutClass() {
+  const heroImage = document.querySelector(".hero_img");
+
+  if (heroImage) {
+    const scrollPosition = window.scrollY;
+    const triggerPosition = document
+      .querySelector(".hero")
+      .getBoundingClientRect().bottom;
+
+    if (scrollPosition > triggerPosition) {
+      // Introduce a delay of 1000 milliseconds (1 second) before applying the fade-out effect
+      setTimeout(function () {
+        heroImage.classList.add("fade-out");
+      }, 500); // Adjust the delay duration as needed
+    } else {
+      heroImage.classList.remove("fade-out");
+    }
+  }
+}
+
+// Attach the scroll event listener
+window.addEventListener("scroll", addFadeOutClass);
+
+// Call the function once to handle the initial state
+addFadeOutClass();
